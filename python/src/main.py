4ec3c7e3b8c699ec829de01c96cb17aef7c4934a
Alan W <awangc31s@gmail.com>
2025-04-16 04:15:50 -0400

    Push

---
+++ python/src/main.py
@@ -0,0 +1,31 @@
+import json
+import sys
+from pathlib import Path
+from model_timer import Timer
+from ipinstance import IPInstance
+
+
+def main(filepath: str):
+
+    filename = Path(filepath).name
+    watch = Timer()
+    watch.start()
+    solver = IPInstance(filepath)
+    inst_str = solver.toString()
+    result = solver.solve()
+    watch.stop()
+    # print(inst_str)
+
+    sol_dict = {
+        "Instance": filename,
+        "Time": str(watch.getElapsed()),
+        "Result": result,
+        "Solution": "OPT",
+    }
+    print(json.dumps(sol_dict))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <input_file>")
+    main(sys.argv[1])
